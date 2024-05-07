780e0e3b059e0de95fa5a92eeab3316b0b58778c
NateLanza <58234814+NateLanza@users.noreply.github.com>
2024-05-07 15:43:46 -0600

    Update main.tsx; revert prev

--- apps/react-trrack-example/src/main.tsx
+++ apps/react-trrack-example/src/main.tsx
@@ -1,5 +1,4 @@
 import { StrictMode } from 'react';
-import { HashRouter } from "react-router-dom";
 import * as ReactDOM from 'react-dom/client';
 
 import App from './app/App';
@@ -8,9 +7,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <HashRouter base="/">
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </HashRouter>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
