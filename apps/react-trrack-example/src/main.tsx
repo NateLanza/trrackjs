e6621657b70568a1b92f24a274899f6e8f7bc7ac
NateLanza <58234814+NateLanza@users.noreply.github.com>
2024-05-07 15:38:15 -0600

    Update main.tsx; try router

--- apps/react-trrack-example/src/main.tsx
+++ apps/react-trrack-example/src/main.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react';
+import { HashRouter } from "react-router-dom";
 import * as ReactDOM from 'react-dom/client';
 
 import App from './app/App';
@@ -7,7 +8,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <HashRouter base="/">
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </HashRouter>
 );
